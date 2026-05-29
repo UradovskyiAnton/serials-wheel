760ffa65f9d4f7ca8852d92807c9bb9fb358fe85
UradovskyiAnton <shoterandaction99@gmail.com>
2026-05-29 12:43:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,11 +3,19 @@ const ASSETS = [
   '/serials-wheel/',
   '/serials-wheel/index.html',
   '/serials-wheel/manifest.json',
-  '/serials-wheel/icon.svg'
+  '/serials-wheel/icon.svg',
+  '/serials-wheel/sw.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
